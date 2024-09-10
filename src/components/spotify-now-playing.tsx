b5f844f7243c40a569a0e02d8919dc63be854210
Antoni Saputra <antonisaputra049@gmail.com>
2024-09-10 23:49:26 +0700

    fix: max title line 2

--- src/components/spotify-now-playing.tsx
+++ src/components/spotify-now-playing.tsx
@@ -24,14 +24,14 @@ export default function SpotifyNowPlaying() {
           ? data.songUrl
           : 'https://open.spotify.com/user/31rn7zt33qzljgvqmiklfkcaxcle'
       }
-      className='relative flex w-72 items-center space-x-4 rounded-md border p-5 transition-shadow hover:shadow-md'
+      className='relative flex w-72 items-center space-x-4 rounded-md border p-2 transition-shadow hover:shadow-md'
     >
       <div className='w-16'>
         {data?.isPlaying ? (
           <Image
             width={200}
             height={200}
-            className='w-16 shadow-sm'
+            className='w-16 shadow-sm rounded-md'
             src={data?.albumImageUrl}
             alt={data?.album}
           />
@@ -46,7 +46,7 @@ export default function SpotifyNowPlaying() {
       </div>
 
       <div className='flex-1'>
-        <p className='text-foreground font-bold'>
+        <p className='text-foreground font-bold text-sm line-clamp-2'>
           {data?.isPlaying ? data.title : 'Not Listening'}
         </p>
         <p className='text-foreground text-xs'>
